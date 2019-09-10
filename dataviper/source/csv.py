894a637da6bbdb1e33b251855766ba100992d7b9
Hiromu OCHIAI <otiai10@gmail.com>
2019-09-10 17:26:53 -0300

    Implement "profile" for CSV

--- dataviper/source/csv.py
+++ dataviper/source/csv.py
@@ -1,7 +1,8 @@
 import os
 import ast
 import pandas as pd
-import operator
+import numpy as np
+import logging
 from dataviper.source.datasource import DataSource
 from dataviper.report.profile import Profile
 from dataviper.logger import IndentLogger
@@ -9,6 +10,7 @@ from dataviper.logger import IndentLogger
 class CSV(DataSource):
 
     def __init__(self, logger=IndentLogger()):
+        logging.warning('NOTE: CSV calc might require much memory of your computer.')
         self.logger = logger
 
 
@@ -18,6 +20,7 @@ class CSV(DataSource):
             rawdata = pd.read_csv(csv_file_name, header=0, usecols=cols)
         else:
             rawdata = pd.read_csv(csv_file_name)
+        # rawdata = rawdata.replace(dict((col, {np.nan: None}) for col in rawdata.columns))
         schema_df = pd.DataFrame(index=rawdata.columns, columns=['data_type'])
         for column in rawdata.columns:
             self.logger.enter('START', 'infer_data_type', column)
@@ -26,6 +29,7 @@ class CSV(DataSource):
         table_name = os.path.basename(os.path.splitext(csv_file_name)[0])
         profile = Profile(table_name, schema_df)
         profile.rawdata = rawdata
+        self.count_total(profile)
         self.logger.exit('DONE', 'get_schema')
         return profile
 
@@ -42,28 +46,60 @@ class CSV(DataSource):
 
 
     def count_total(self, profile):
+        profile.total = len(profile.rawdata)
         return profile
 
 
     def count_null(self, profile):
+        profile.schema_df['null_count'] = 0.0
+        profile.schema_df['null_%'] = 0.0
+        for col in profile.rawdata.columns:
+            null_count = self.__count_null_for(col, profile.rawdata[col])
+            profile.schema_df.at[col, 'null_count'] = null_count
+            profile.schema_df.at[col, 'null_%'] = float(null_count / profile.total) * 100
+        return profile
+
+
+    def __count_null_for(self, col, values):
+        return values.isna().sum()
+
+
+    def count_unique(self, profile):
+        profile.schema_df['unique_count'] = 0.0
+        profile.schema_df['unique_%'] = 0.0
         return profile
 
 
     def get_deviation(self, profile):
+        profile.schema_df['min'] = np.nan
+        profile.schema_df['max'] = np.nan
+        profile.schema_df['avg'] = np.nan
+        profile.schema_df['med'] = np.nan
+        profile.schema_df['std'] = np.nan
+        for col in profile.rawdata.columns:
+            if profile.schema_df['data_type'][col] not in ('int', 'float'):
+                continue
+            profile.schema_df.at[col, 'min'] = profile.rawdata[col].min()
+            profile.schema_df.at[col, 'max'] = profile.rawdata[col].max()
+            profile.schema_df.at[col, 'avg'] = profile.rawdata[col].mean()
+            profile.schema_df.at[col, 'med'] = profile.rawdata[col].median()
+            profile.schema_df.at[col, 'std'] = profile.rawdata[col].std()
         return profile
 
 
-    def count_unique(self, profile):
+    def get_examples(self, profile, count=5):
+        profile.schema_df['top_{}_examples'.format(count)] = [[]] * len(profile.schema_df)
+        profile.schema_df['last_{}_examples'.format(count)] = [[]] * len(profile.schema_df)
+        for col in profile.rawdata.columns:
+            profile.schema_df.at[col, 'top_{}_examples'.format(count)]  = profile.rawdata[col].head(n=count).tolist()
+            profile.schema_df.at[col, 'last_{}_examples'.format(count)] = profile.rawdata[col].tail(n=count).tolist()
         return profile
 
 
-    def get_examples(self, profile):
+    def pivot(self, profile, key, categorical_columns, result_table, **kwargs):
+        logging.warning("CSV pivot is not supported yet.")
         return profile
-
-
-    def pivot(self, profile):
-        return profile
-
 
     def joinability(self, on):
+        logging.warning("CSV joinability check is not supported yet.")
         return
--- tests/story_test.py
+++ tests/story_test.py
@@ -1,12 +1,17 @@
 import os
-import tempfile
+import shutil
+from pathlib import Path
 from dataviper.client import Client
 from dataviper.source import CSV
 
 def test_story_csv():
-    outdir = tempfile.gettempdir()
+    outdir = os.path.join(os.path.dirname(__file__), 'data', 'out')
+    shutil.rmtree(outdir, ignore_errors=True)
+    Path(outdir).mkdir(exist_ok=True, parents=True)
     filename = os.path.join(os.path.dirname(__file__), 'data', 'sales_data.csv')
     client = Client(source=CSV())
     profile = client.profile(filename)
     assert profile.table_name == 'sales_data'
     profile.to_excel(outdir=outdir)
+    client.pivot(profile, 'id', ['region', 'sales_type'])
+
