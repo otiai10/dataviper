6f33a924d1580b317a3c43a54a3060ab3201cd44
Hiromu Ochiai <otiai10@gmail.com>
2020-04-20 01:12:44 +0900

    Revert "Suppress warning on pymysql"
    This reverts commit 510bf73cb9362b200929f7faa24a6d0540bca087.

--- tests/source/mysql_test.py
+++ tests/source/mysql_test.py
@@ -18,15 +18,11 @@ def fixture_Sales_table():
     lines = []
     with open(sql_file_path) as sql_file:
         lines = sql_file.read().split(';')
-    conn = pymysql.connect(**__MySQL_CONFIG__)
-    try:
-        with conn.cursor() as cur:
-            for line in lines:
-                cur.execute(line)
-            yield
-    finally:
-        # conn.cursor().execute('DROP TABLE Sales')
-        conn.close()
+    with pymysql.connect(**__MySQL_CONFIG__) as conn:
+        for line in lines:
+            conn.execute(line)
+        yield
+        conn.execute('DROP TABLE Sales')
 
 
 def test_MySQL_profile(fixture_Sales_table):
