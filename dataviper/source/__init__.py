667d28a2229180b584028432118fdc37516239c0
Hiromu Ochiai <otiai10@gmail.com>
2020-04-20 01:05:13 +0900

    Add small test story

--- dataviper/source/__init__.py
+++ dataviper/source/__init__.py
@@ -1,2 +1,3 @@
 # flake8: noqa
+from dataviper.source.csv import CSV
 from dataviper.source.mysql import MySQL
--- tests/story_test.py
+++ tests/story_test.py
@@ -1,11 +1,32 @@
 import os
 import shutil
+import pytest
 from pathlib import Path
-from dataviper.client import Client
-from dataviper.source.csv import CSV
+from dataviper import Client
+from dataviper.source import CSV
+from dataviper.source import MySQL
+
+__MySQL_CONFIG__ = {
+    'user': os.getenv('MYSQL_USER', 'root'),
+    'password': os.getenv('MYSQL_PASSWORD', os.getenv('MYSQL_ROOT_PASSWORD', '')),
+    'host': os.getenv('MYSQL_HOST', 'localhost'),
+    'port': int(os.getenv('MYSQL_PORT', 3306)),
+    'database': os.getenv('MYSQL_DATABASE', 'dataviper_test')
+}
 
 
-def test_story_csv():
+def get_outdir():
+    return os.path.join(os.path.dirname(__file__), 'data', 'out')
+
+
+@pytest.fixture
+def fixture_outdir():
+    outdir = get_outdir()
+    # shutil.rmtree(outdir, ignore_errors=True)
+    Path(outdir).mkdir(exist_ok=True, parents=True)
+    yield
+
+def test_story_csv(fixture_outdir):
     outdir = os.path.join(os.path.dirname(__file__), 'data', 'out')
     shutil.rmtree(outdir, ignore_errors=True)
     Path(outdir).mkdir(exist_ok=True, parents=True)
@@ -15,3 +36,12 @@ def test_story_csv():
     assert profile.table_name == 'sales_data'
     profile.to_excel(outdir=outdir)
     client.pivot(profile, 'id', ['region', 'sales_type'])
+
+
+def test_story_MySQL(fixture_outdir):
+    outdir = get_outdir()
+    client = Client(source=MySQL(__MySQL_CONFIG__))
+    with client.connect():
+        profile = client.profile("Sales")
+        profile.to_excel(outdir=outdir)
+        profile.to_csv(outdir=outdir)
